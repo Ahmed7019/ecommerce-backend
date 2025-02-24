8bbaf45ac88e696d72477c6d881f66db5ab5381e
Ahmed Abdallah <76991196+Ahmed7019@users.noreply.github.com>
2025-02-24 12:00:28 +0200

    feat: added a logger to the server

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ import express from "express";
 import cors from "cors";
 import products from "./Routes/products.js";
 import bodyParser from "body-parser";
-import multer from "multer";
-
+import logger from "./middleware/logger.js";
 const PORT = process.env.PORT || 8080;
 const app = express();
-const upload = multer();
-// app.use(cors(corsOptions));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger);
 app.use("/api/products", products);
-
 app.listen(PORT, () => console.log(`Server is running on port => ${PORT}`));
